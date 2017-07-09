c9795778484e51db4ff85165b49e51efbce32942
Chad Knight <chadrknight@gmail.com>
2017-07-08 19:02:55 -0500

    reading files

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,29 @@
 module Main where
 
+import Data.Time.Calendar (showGregorian)
+import qualified Data.Dates as Dates
+import System.IO (readFile)
+import System.IO.Error (isDoesNotExistError)
+import Control.Exception (tryJust)
+import Control.Monad (guard)
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  -- get the current date (IO from system clock?)
+  today <- Dates.getCurrentDateTime
+  -- figure out what the previous day was
+  let lastday = getLastWorkDay today
+  msg <- getMessage (makeFileName lastday)
+  putStrLn $ msg
+
+getLastWorkDay :: Dates.DateTime -> Dates.DateTime
+getLastWorkDay currentDate =
+  Dates.minusInterval currentDate (Dates.Days 1)
+
+makeFileName :: Dates.DateTime -> String
+makeFileName date = showGregorian (Dates.dateTimeToDay date) ++ ".plan"
+
+getMessage :: String -> IO String
+getMessage fileName = do
+  f <- tryJust (guard . isDoesNotExistError) $ readFile fileName
+  return (either (const "You didn't make a plan for yesterday.") id f)
